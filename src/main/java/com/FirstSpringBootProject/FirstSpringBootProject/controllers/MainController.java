d244def93fe522dace47c5d5767d419d2f4650b4
PoroshinGA <119220049+PoroshinGA@users.noreply.github.com>
2023-05-23 23:02:50 +0300

    add machine learning model and some features

--- src/main/java/com/FirstSpringBootProject/FirstSpringBootProject/controllers/MainController.java
+++ src/main/java/com/FirstSpringBootProject/FirstSpringBootProject/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String home(Model model) {
-        return "home";
+        return "buffer";
     }
     @GetMapping("/info")
     public String info(Model model){
